fbb09d0e272693e80ee31dd926b81522a1540c8f
Sabrina Ibarra <sabrina@Sabrinas-MacBook-Pro.local>
2023-03-07 21:55:39 -0500

    Add colors for colorschemes.

---
+++ colorschemes/colors.py
@@ -0,0 +1,12 @@
+"""List valid colors for colorschemes."""
+from enum import Enum
+
+
+class Color(Enum):
+    """List valid colors for colorschemes."""
+    PINK = (232, 30, 134)
+    GREEN = (89, 237, 14)
+    BLUE = (20, 137, 219)
+    YELLOW = (245, 225, 5)
+    ORANGE = (245, 121, 5)
+    CLEAR = (255, 255, 255)
